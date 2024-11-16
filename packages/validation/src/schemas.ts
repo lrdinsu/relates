da23459c13c2da4af9fcb8b8f8222fc7e3e96619
lilia <143701282+lrdinsu@users.noreply.github.com>
2024-11-15 19:08:01 -0800

    chore: update eslint config

--- packages/validation/src/schemas.ts
+++ packages/validation/src/schemas.ts
@@ -43,19 +43,21 @@ export const LoginSchema = BaseUserSchema.pick({
   password: true,
 });
 
-// PostSchema
 export const BasePostSchema = z.object({
   postedBy: ObjectIdSchema,
   text: z.string().max(500).optional(),
   img: z.string().optional(),
   likes: z.array(ObjectIdSchema).default([]),
   commentsCount: z.number().default(0),
+  parentPost: ObjectIdSchema.nullable().optional(),
+  isDeleted: z.boolean().default(false),
 });
 
 export const PostCreateSchema = BasePostSchema.omit({
   postedBy: true,
   likes: true,
   commentsCount: true,
+  isDeleted: true,
 }).refine((data) => Boolean(data.text) || Boolean(data.img), {
   message: "At least 'text' or 'img' must be provided.",
   path: ['text', 'img'],
@@ -68,23 +70,3 @@ export const PostUpdateSchema = BasePostSchema.partial().refine(
     path: ['text', 'img'],
   },
 );
-
-// CommentSchema
-export const BaseCommentSchema = z.object({
-  postId: ObjectIdSchema,
-  userId: ObjectIdSchema,
-  username: z.string(),
-  profilePic: z.string().nullable().default(null),
-  text: z.string().min(2).max(280),
-  likes: z.array(ObjectIdSchema).default([]),
-  parentCommentId: ObjectIdSchema.nullable().optional(),
-  repliesCount: z.number().default(0),
-});
-
-export const CommentCreateSchema = BaseCommentSchema.pick({
-  text: true,
-});
-
-export const CommentUpdateSchema = BaseCommentSchema.pick({
-  text: true,
-});
--- packages/validation/src/types.ts
+++ packages/validation/src/types.ts
@@ -1,13 +1,7 @@
 import { z } from 'zod';
 
-import {
-  BaseCommentSchema,
-  BasePostSchema,
-  LoginSchema,
-  UserSchema,
-} from './schemas.js';
+import { BasePostSchema, LoginSchema, UserSchema } from './schemas.js';
 
 export type UserType = z.infer<typeof UserSchema>;
 export type LoginType = z.infer<typeof LoginSchema>;
 export type PostType = z.infer<typeof BasePostSchema>;
-export type CommentType = z.infer<typeof BaseCommentSchema>;
