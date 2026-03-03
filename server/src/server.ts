d6fed410c959d01a0e109928f0cbe52bdef3c628
lilia <143701282+lrdinsu@users.noreply.github.com>
2026-03-03 06:57:33 -0800

    chore: Docker setup, and Nginx configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,8 +18,8 @@ process.on('uncaughtException', (err) => {
   });
 });
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started at ${PORT}`);
 });
